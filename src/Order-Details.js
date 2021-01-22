8257706345f6f1d45ac275480c8d35e3f4bc187a
Akinyemi Olalekan <69955888+primus02@users.noreply.github.com>
2021-01-22 09:42:40 +0000

    Update Order-Details.js

--- src/Order-Details.js
+++ src/Order-Details.js
@@ -1,15 +1,35 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+import  {toast, ToastContainer, Zoom} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 import "./App.css";
-
+const google = window.google ? window.google : {};
 
 
 function OrderDetails(props){
 
+    const [newDestination, setNewDestination]= useState("");
+
     useEffect(()=>{
         getOrder();
-    });
+
+        let  autocomplete = new google.maps.places.Autocomplete(
+            (document.getElementsByClassName('new-destination')[0]),
+    
+            { types: ['geocode']});
+    
+    
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+    
+               completeAddress(autocomplete);
+    
+        });
+    
+        const completeAddress=()=>{
+            setNewDestination(document.getElementsByClassName('new-destination')[0].value);
+          }
+    }, []);
 
     const username = localStorage.getItem("username");
     const url = "https://sendit.herokuapp.com";
@@ -37,6 +57,82 @@ function OrderDetails(props){
         });
     };
 
+    const changeDestination=()=>{
+        document.querySelector(".dest-modal").classList.remove("d-none");
+		
+		document.querySelector(".edit").setAttribute("disabled", "");
+	    document.querySelector(".cancel").setAttribute("disabled", "");
+		
+    };
+
+    const updatedDestination=(e)=>{
+        setNewDestination(e.target.value);
+    };
+
+    const updateNewDestination=()=>{
+         
+        if(newDestination !==""){
+			
+            fetch(`${url}/change-destination/search?username=${username}&id=${props.match.params.id}`, {
+            method: "PATCH",
+            headers: {
+            "Content-Type": "application/json", 
+             Authorization: `Bearer ${token}`
+           },
+           body: JSON.stringify({
+            destination: newDestination
+          })
+    })
+    .then(res=> res.json())
+    .then(res=>{
+        if(res.message=== `Order ${props.match.params.id} updated successfully`){
+            
+           toast.success("Success, your order's new destination updated");
+            
+            setTimeout(()=>{ props.history.push("/all-orders");}, 3000);
+            
+        }
+        
+    })
+    .catch(err=>{
+        console.log("Error", err);
+    });
+            }
+            else{
+                window.location.reload();
+            }
+
+    };
+
+    const cancelOrder=()=>{
+        const decision= prompt("Are you sure you want to cancel this order? Confirm this by typing any word in the space below and click OK!");
+		if(decision){
+			fetch(`${url}/cancel-order/search?username=${username}&id=${props.match.params.id}`, {
+            method: "DELETE",
+            headers: {
+		   "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+    }
+})
+.then(res=> res.json())
+.then(res=> {
+    if(res.success){
+		
+       toast.success("Success, your order has been cancelled");
+        
+		setTimeout(()=>{ props.history.push("/all-orders");}, 3000);
+        
+    }
+})
+.catch(err=> {
+		console.log("Error", err);
+	});
+		}
+		else{
+			return;
+		}
+    };
+
     const signOut=()=>{
         localStorage.clear();
         props.history.push("/");
@@ -51,12 +147,22 @@ function OrderDetails(props){
                     <li><Link to="/create-order">Create Order</Link></li>
                     <li><Link to="/all-orders">Get all orders</Link></li>
                     <li className="sign-out" onClick={signOut}>Sign out</li>
+                    <ToastContainer draggable={false} transition={Zoom} autoClose={3000}/>
                 </ul>
             </nav>
         
         
         
             <div className="main-body">
+
+                <div className="dest-modal d-none">
+                    <p>Kindly provide the new destination for your order below and click on the submit button</p>
+                    
+                    <input type="text" className="new-destination" placeholder="Enter new destination" value={newDestination} onChange={updatedDestination}/>
+                    
+                    <p><button onClick={updateNewDestination}>Submit</button></p>
+                </div>
+
                 <header>
                     <h1> Hello <span className="username">{username}</span>!</h1>
                 </header>
@@ -72,17 +178,17 @@ function OrderDetails(props){
                             <p className="order">Order details;</p>
                             
                             <div className="order-info">
-                                <label>Price(#): <span className="price">{order.price} </span></label><br></br>
+                                <label>Price(#): </label><span className="price">{order.price} </span><br></br>
 
-                                <label>Pick-Up Location: <span className="location">{order.location}</span></label><br></br>
+                                <label>Pick-Up Location: </label><span className="pick-location">{order.location}</span><br></br>
 
-                                <label>Destination: <span className="destination">{order.destination}</span></label><br></br>
+                                <label>Destination: </label><span className="destination">{order.destination}</span><br></br>
 
-                                <label>Recipient Mobile: <span className="mobile">{order.recmobile}</span></label><br></br>
+                                <label>Recipient Mobile: </label><span className="mobile">{order.recmobile}</span><br></br>
 
-                                <label>Date of creation: <span className="date">{order.date}</span></label>
+                                <label>Date of creation: </label><span className="date">{order.date}</span>
 
-                                {order.status === "pending" ? <p><button className="edit"><Link to={`/change-destination/${order._id}`}>Edit-Destination</Link></button><button className="cancel"><Link to={`/cancel-order/${order._id}`}>Cancel-Order</Link></button></p> 
+                                {order.status === "pending" ? <p><button className="edit" onClick={changeDestination}>Edit-Destination</button><button className="cancel" onClick={cancelOrder}>Cancel-Order</button></p> 
                                 : <p className="delivered">Status: Delivered</p>}
                             </div>
                     </div>

3259cf2035f85c70394249c708d2298d8d05cee8
Akinyemi Olalekan <69955888+primus02@users.noreply.github.com>
2021-01-22 09:38:45 +0000

    Update Get-All-App-Orders.js

--- src/Get-All-App-Orders.js
+++ src/Get-All-App-Orders.js
@@ -1,15 +1,36 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
+import  {toast, ToastContainer, Zoom} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 import "./App.css";
-
+const google = window.google ? window.google : {};
 
 
 function GetAllAppOrders(props){
 
+   const [newLocation, setNewLocation]=useState("");
+
     useEffect(()=>{
        getAllAppOrders();
+
+       let  autocomplete = new google.maps.places.Autocomplete(
+        (document.getElementsByClassName('new-location')[0]),
+
+        { types: ['geocode']});
+
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+
+           completeAddress(autocomplete);
+
     });
+
+    const completeAddress=()=>{
+        setNewLocation(document.getElementsByClassName('new-location')[0].value);
+      }
+
+    },[]);
 
     const username = localStorage.getItem("username");
     const url = "https://sendit.herokuapp.com";
@@ -40,6 +61,97 @@ function GetAllAppOrders(props){
         });
     };
 
+    const changeStatus=(orderId)=>{
+        const newStatus = prompt("Kindly enter the new status of this order");
+			
+			if(newStatus){
+            
+			 fetch(`${url}/change-status/${orderId}`, {
+              method: "PATCH",
+              headers: {
+               "Content-Type": "application/json", 
+               Authorization: `Bearer ${token}`
+              },
+             body: JSON.stringify({
+              status: newStatus.toLowerCase()
+             })
+           })
+           .then(res=> res.json())
+           .then(res=>{
+             if(res.message=== `Order ${orderId} updated successfully`){
+		
+             toast.success("Success, order's new status updated!");
+        
+		     setTimeout(()=>{ window.location.reload();}, 3000);
+        
+           }
+    
+           })
+           .catch(err=>{
+            console.log("Error", err);
+         });
+	    }
+         else{
+		 return;
+	    } 
+    };
+
+    const changeLocation=(orderId)=>{
+        document.querySelector(".location-modal").classList.remove("d-none");
+        localStorage.setItem("id", orderId);
+        document.querySelectorAll(".orders-holder button").forEach((button)=>{
+            button.setAttribute("disabled", "");
+        });
+
+        document.querySelectorAll(".orders-holder span").forEach((span)=>{
+            span.setAttribute("disabled", "");
+        });
+
+        document.querySelector(".orders-holder").classList.add("d-none");
+
+         };
+
+    const updatedLocation=(e)=>{
+          setNewLocation(e.target.value);
+    };
+
+    const updateNewLocation=()=>{
+
+        if(newLocation !==""){
+            const orderId = localStorage.getItem("id");
+
+            document.querySelector(".location-modal").classList.add("d-none");
+            
+            fetch(`${url}/change-location/${orderId}`, {
+              method: "PATCH",
+              headers: {
+               "Content-Type": "application/json", 
+               Authorization: `Bearer ${token}`
+              },
+              body: JSON.stringify({
+                preslocation: newLocation
+              })
+            })
+            .then(res=> res.json())
+            .then(res=>{
+               if(res.message=== `Order ${orderId} updated successfully`){
+               toast.success("Success, order's present location updated!");
+      
+                setTimeout(()=>{ window.location.reload();}, 3000);
+     
+             }
+  
+          })
+           .catch(err=>{
+             console.log("Error", err);
+          });
+          }
+          else{
+              window.location.reload();
+          }
+    };
+
+
     const signOut=()=>{
         localStorage.clear();
         props.history.push("/");
@@ -67,10 +179,21 @@ function GetAllAppOrders(props){
                     <li><Link to="/admin-profile">Profile</Link></li>
                     <li><Link to="#">Create Order</Link></li>
                     <li className="sign-out" onClick={signOut}>Sign out</li>
+                    <ToastContainer draggable={false} transition={Zoom} autoClose={3000}/>
                 </ul>
             </nav>
             
             <div className="main-body">
+
+            <div className="location-modal d-none">
+		       <p>Kindly provide the new location for this order below and click on the submit button</p>
+			   
+			   <input type="text" className="new-location" placeholder="Enter new location" value={newLocation} onChange={updatedLocation}/>
+			   
+			   <p><button onClick={updateNewLocation}>Submit</button></p>
+		   </div>
+
+
                 <header>
                     <h1> Hello Admin <span className="username">{username}</span>!</h1>
                 </header>
@@ -90,8 +213,8 @@ function GetAllAppOrders(props){
                     <div className="orders-holder">
                         {orders.length<1 ? " " : orders.map(order=>(
                             order.price ? 
-                            <div className="orders" key={order._id}>  
-                            <p className="order"><span className="order-username">{order.username}</span><button className="details-btn"><Link to={`/app-order/${order._id}`}>Details</Link></button><button className="status"><Link to={`/order-status/${order._id}`}>Status: <span className={order.status==="pending" ? "red" : "status-state"} >{order.status}</span></Link></button><button className="location"><Link to={`/order-location/${order._id}`}>Pres-Location: <span className="present-location" >{order.preslocation}</span></Link></button></p> 
+                        <div className="orders" key={order._id}>  
+                            <p className="order"><span className="order-username">{order.username}</span><button className="details-btn"><Link to={`/app-order/${order._id}`}>Details</Link></button><button className="status" onClick={()=>{changeStatus(order._id)}}>Status: <span className={order.status==="pending" ? "red" : "status-state"} >{order.status}</span></button><button className="location" onClick={()=>{changeLocation(order._id)}}>Pres-Location: <span className="present-location" >{order.preslocation}</span></button></p> 
                          </div> : ""
                         )) }
                         
